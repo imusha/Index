b670698d0aa6167218baa5762d249f280f7f3376
p usha <imusha24@outlook.com>
2017-09-15 23:48:59 +0530

    new text file

---
+++ Html Practice/test.js
@@ -0,0 +1,40 @@
+document.write("&nbsp &nbsp&nbsp&nbsp USE OF PRINT STATEMENT");
+
+document.write("hello");   //used to print anything on the screen
+document.write("<br>");   //use of break statement for new line.
+alert("check the error!!");   //shows the alert statement
+document.write("&nbsp &nbsp&nbsp&nbsp USE OF FUNCTIONS STATEMENT");
+
+function addvalue(num,str){   // use of function and passing variable into the function
+  var add= num + str;
+  document.write(add);
+  alert(add);
+  }
+  addvalue(22, " need to improve");   //the value returned.
+  document. write(".........................................................");
+  document.write("<br>");
+  var num1= 22;
+  var num2= 20;
+  var num1= num1 + num2;
+  //var num1+= num2;
+  var num = 4+2*3;
+  document. write(num);
+  document.write("<br>");
+  document. write(num1);
+  document.write("<br>");
+  //document.write("<br>");
+  document. write(".........................................................");
+  document.write("<br>");
+
+    document.write("&nbsp &nbsp&nbsp&nbsp USE OF IF ELSE STATEMENT");
+    document.write("<br>");
+    document.write("<br>");
+    var food= "apple";
+    if(food=="apple"){
+      document.write("Its good for health");
+      alert("Its good for health");
+        }
+    else{
+      document.write("its not apple");
+      alert("Its not apple");
+    }
