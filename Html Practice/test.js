e529c2b21e1436fc1e2812705d15d3a93667f6c2
p usha <imusha24@outlook.com>
2017-09-16 00:34:34 +0530

    new text file

--- Html Practice/test.js
+++ Html Practice/test.js
@@ -1,17 +1,38 @@
-document.write("&nbsp &nbsp&nbsp&nbsp USE OF PRINT STATEMENT");
+// document.write("&nbsp &nbsp&nbsp&nbsp USE OF PRINT STATEMENT");
+// document.write("<br>");
+//
+// document. write(".........................................................");
+// document.write("<br>");
+// document.write("<br>");
 
-document.write("hello");   //used to print anything on the screen
+document.write("&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp HELLO");   //used to print anything on the screen
 document.write("<br>");   //use of break statement for new line.
 alert("check the error!!");   //shows the alert statement
-document.write("&nbsp &nbsp&nbsp&nbsp USE OF FUNCTIONS STATEMENT");
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write("&nbsp &nbsp&nbsp&nbsp USE OF FUNCTIONS STATEMENT");
+// document.write("<br>");
+//
+// document. write("&nbsp &nbsp &nbsp.........................................................");
+//
+// document.write("<br>");
+// document.write("<br>");
 
 function addvalue(num,str){   // use of function and passing variable into the function
   var add= num + str;
   document.write(add);
   alert(add);
   }
-  addvalue(22, " need to improve");   //the value returned.
-  document. write(".........................................................");
+  addvalue(22, " need to improve");
+  document.write("<br>");
+
+  // document.write("<br>");
+  // document.write("<br>");
+  //   //the value returned.
+  // document. write(".........................................................");
+  // document.write("<br>");
+  //
   document.write("<br>");
   var num1= 22;
   var num2= 20;
@@ -23,11 +44,11 @@ function addvalue(num,str){   // use of function and passing variable into the f
   document. write(num1);
   document.write("<br>");
   //document.write("<br>");
-  document. write(".........................................................");
-  document.write("<br>");
-
-    document.write("&nbsp &nbsp&nbsp&nbsp USE OF IF ELSE STATEMENT");
-    document.write("<br>");
+  // document. write(".........................................................");
+  // document.write("<br>");
+  //
+  //   document.write("&nbsp &nbsp&nbsp&nbsp USE OF IF ELSE STATEMENT");
+  //   document.write("<br>");
     document.write("<br>");
     var food= "apple";
     if(food=="apple"){
